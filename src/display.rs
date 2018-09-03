e10e961ce29bfa0de76309f31dba1316461acec5
Tom Arrell <tom.arrell@gmail.com>
2018-09-03 20:04:25 +1200

    Initial commit

---
+++ src/display.rs
@@ -0,0 +1,56 @@
+use std::path::PathBuf;
+
+#[macro_use]
+use std::fmt;
+
+pub struct Node {
+    pub depth: usize,
+    pub path: PathBuf,
+    pub children: Vec<Node>,
+}
+
+pub fn print_node(node: Node) -> String {
+    let optional_path = match node.path.file_name() {
+        Some(name) => name.to_str(),
+        None => return String::new(),
+    };
+
+    let path_name = match optional_path {
+        Some(name) => name,
+        None => panic!("Failed to decode path name"),
+    };
+
+    format!("{:spaces$}├─── {}", "", path_name, spaces=(node.depth * 4))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn print_node_works() {
+        let node = Node {
+            depth: 1,
+            path: PathBuf::from("/test/filename.rs"),
+            children: vec![],
+        };
+
+        assert_eq!("    ├─── filename.rs", print_node(node)) ;
+    }
+
+    #[test]
+    fn print_node_works_width_depth() {
+        let node = Node {
+            depth: 3,
+            path: PathBuf::from("/test/otherfile.rs"),
+            children: vec![],
+        };
+
+        assert_eq!("            ├─── otherfile.rs", print_node(node)) ;
+    }
+
+    #[test]
+    fn it_works() {
+        assert_eq!("Hello", "Hello");
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+mod display;
+
+fn main() {}
+
